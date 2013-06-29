cb383c312b55465674a96c0dd63480b7b5471cfa
Skrzypek <maskrz@gmail.com>
2013-06-29 19:18:57 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PersonalTrainer::Application.config.secret_key_base = '9aac0d51a8999ef4f4cab1152163faac95e59687a068704820cf19cc150a0faca669bb8d88dd5ef503689c0bf67876d25fccc71444f2aea449518eb2836b93d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PersonalTrainer::Application.config.session_store :cookie_store, key: '_PersonalTrainer_session'
